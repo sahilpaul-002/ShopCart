46915a23560fe2861eea284350cbd1ec59be6335
Sahil Paul <sahil.p@stylopay.com>
2025-08-14 00:31:05 +0530

    Deleted console.log

--- Backend/utils/AuthenticateToken.js
+++ Backend/utils/AuthenticateToken.js
@@ -7,9 +7,7 @@ const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 const authenticateToken = (token) => {
     try {
-        console.log(token);
         const decoded = JWT.verify(token, jwtSecretKey);
-        console.log('Decoded token:', decoded);
         return true;
     } catch (err) {
         // Token is invalid or expired → reject request
