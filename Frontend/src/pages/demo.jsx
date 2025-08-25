baa90e23d2984a7c09adf913698e0a6e0e72ce4d
Sahil Paul <sahil.p@stylopay.com>
2025-08-25 08:37:10 +0530

    Started fixing product details page respnsive and flex

--- Frontend/src/pages/demo.jsx
+++ Frontend/src/pages/demo.jsx
@@ -45,8 +45,8 @@ function ProductDetail() {
 
     return productData ? (
         <div >
-            <div className=' w-[100vw] h-[200vh] md:h-[150vh] lg:min-h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] flex flex-col justify-start items-center md:flex-row md:items-start md:justify-center  gap-[20px] pt-[150px] md:pt-[100px] '>
-                <div className='lg:w-[50vw] md:w-[90vw] md:h-[90vh] h-[50vh]  flex items-center justify-center md:gap-[10px] gap-[30px] flex-col-reverse lg:flex-row'>
+            <div className=' w-[99vw] h-[130vh] md:h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] flex items-center justify-start flex-col lg:flex-row gap-[20px]'>
+                <div className='lg:w-[50vw] md:w-[90vw] lg:h-[90vh] h-[50vh] mt-[70px] flex items-center justify-center md:gap-[10px] gap-[30px] flex-col-reverse lg:flex-row'>
                     <div className='lg:w-[20%] md:w-[80%] h-[10%] lg:h-[80%] flex items-center justify-center gap-[50px] lg:gap-[20px] lg:flex-col flex-wrap '>
                         <div className='md:w-[100px]  w-[50px] h-[50px] md:h-[110px] bg-slate-300 border-[1px] border-[#80808049] rounded-md'>
                             <img src={image1} alt="" className='w-[100%] h-[100%]  cursor-pointer rounded-md' onClick={() => setImage(image1)} />
@@ -67,8 +67,8 @@ function ProductDetail() {
                     </div>
                 </div>
 
-                <div className='lg:w-[50vw] w-[100vw] h-[60vh]  md:h-[120vh] lg:min-h-[96vh]  flex items-start justify-start flex-col pt-[40px] px-[30px] md:pb-[20px] md:pl-[20px] gap-[10px]'>
-                    <h1 className='text-[40px] font-semibold !text-[aliceblue]'>{productData.name.toUpperCase()}</h1>
+                <div className='lg:w-[50vw] w-[100vw] lg:h-[75vh] h-[40vh] lg:mt-[80px] flex items-start justify-start flex-col py-[20px] px-[30px] md:pb-[20px] md:pl-[20px] lg:pl-[0px] lg:px-[0px] lg:py-[0px] gap-[10px]'>
+                    <h1 className='text-[40px] font-semibold text-[aliceblue]'>{productData.name.toUpperCase()}</h1>
                     <div className='flex items-center gap-1 '>
                         <FaStar className='text-[20px] fill-[#FFD700]' />
                         <FaStar className='text-[20px] fill-[#FFD700]' />
